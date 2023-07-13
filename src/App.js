ffadc14b37b39c0d99716ce1718efd3f0ce4e3cb
Ht-atho <thanh.opentechiz@gmail.com>
2023-07-13 11:09:16 +0700

    abcd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Component/Home.jsx'
+import {Routes, Route, Link} from "react-router-dom"
+import About from './Component/About';
+import Contract from './Component/Contract';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>
+        ngu ngu 
+      </h1>
+      <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+              <Link to="/about">About</Link>
+            </li>
+          <li>
+            <Link to="/contract">Contract</Link>
+          </li>
+      </ul>
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contract />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Component/Contract.jsx
@@ -0,0 +1,8 @@
+function Contract(){
+    return(
+       
+            <h1> Contract nay</h1>
+       
+    )
+} 
+export default Contract
